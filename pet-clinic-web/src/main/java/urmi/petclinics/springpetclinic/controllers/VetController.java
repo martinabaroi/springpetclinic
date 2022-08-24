f707c1f86e02896f7cd2c18fb7b4669a69b62fd8
martinabaroi <109995362+martinabaroi@users.noreply.github.com>
2022-08-24 16:23:42 -0400

    vet controller and index page added (#25)

--- pet-clinic-web/src/main/java/urmi/petclinics/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/urmi/petclinics/springpetclinic/controllers/VetController.java
@@ -1,15 +1,29 @@
 package urmi.petclinics.springpetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import urmi.petclinics.springpetclinic.model.Vet;
+import urmi.petclinics.springpetclinic.services.VetService;
+
+
 
 @Controller
 public class VetController {
 	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
 	
 	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
-		
+	public String listVets(Model model) {
+		Set<Vet> vetList = vetService.findAll();
+		model.addAttribute("vets", vetList);
 		
 		return "vets/index";
 	}

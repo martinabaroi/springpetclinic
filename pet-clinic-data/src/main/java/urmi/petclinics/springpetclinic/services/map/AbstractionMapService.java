cbb793bdc16217f57b99b9562118bd4acdbe3192
Martina Baroi <m_baroi@yahoo.com>
2022-08-25 15:47:05 -0400

    auto id method added

--- pet-clinic-data/src/main/java/urmi/petclinics/springpetclinic/services/map/AbstractionMapService.java
+++ pet-clinic-data/src/main/java/urmi/petclinics/springpetclinic/services/map/AbstractionMapService.java
@@ -1,14 +1,20 @@
 package urmi.petclinics.springpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
-public abstract class AbstractionMapService<T, ID>  {
+import javax.management.RuntimeErrorException;
+
+import urmi.petclinics.springpetclinic.model.BaseEntity;
+
+public abstract class AbstractionMapService<T extends BaseEntity, ID extends Long>  { //for auto generic id T must be extends BaseEntity and Id must be extends Long
 	
-	protected Map<ID, T> map = new HashMap<>();			//will create this map automatically when this class's method will call by another class's method.
-	
+	protected Map<Long, T> map = new HashMap<>();			//will create this map automatically when this class's method will call by another class's method.
+	//key must be Long for auto id
 	
 	Set<T> findAll() {
 		Set<T> valuesOfMap = new HashSet<>(map.values());  //this method will return a set of values of map only not keys value.
@@ -20,8 +26,19 @@ public abstract class AbstractionMapService<T, ID>  {
 		return object;
 	}
 	
-	T save(ID id, T object) {
-		map.put(id, object);  	//this method save  key(id) and value(object) in map.
+	T save(T object) {
+		if( object != null) {
+			if(object.getId() == null) { //if we dont give any id when create the object, than it will be null
+				object.setId(getNextId()); //set here id from auto method.
+			}
+			
+			map.put(object.getId(), object); 
+		  	//this method save  key(id) and value(object) in map.
+		}
+		else {	//if Object object3 = null; we make a null object than exception will throw.
+			throw new RuntimeException("object can not be null"); //app will be off because throw this exception didnt catch
+		}
+		
 		return object;
 	}
 	
@@ -31,5 +48,18 @@ public abstract class AbstractionMapService<T, ID>  {
 	
 	void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+	
+	private Long getNextId() { //for auto id this method will call.
+		Long a = null;
+		try {
+			a =Collections.max(map.keySet()) + 1; //max means from the keySet the max number will return and than add 1.
+		}
+		catch(NoSuchElementException e) {  
+			a = 1L; //for first time, if in keyset there will be no id set, than  id will be 1
+		}
+	
+		return  a; 
+		
 	}
 }

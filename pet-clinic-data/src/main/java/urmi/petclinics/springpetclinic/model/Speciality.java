b6161a97cd89c41bfb88a5792fc705ced467c322
Martina Baroi <m_baroi@yahoo.com>
2022-08-29 11:32:29 -0400

    vet speciality entity

---
+++ pet-clinic-data/src/main/java/urmi/petclinics/springpetclinic/model/Speciality.java
@@ -0,0 +1,16 @@
+package urmi.petclinics.springpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+	private String discrption;
+
+	public String getDiscrption() {
+		return discrption;
+	}
+
+	public void setDiscrption(String discrption) {
+		this.discrption = discrption;
+	}
+	
+	
+}

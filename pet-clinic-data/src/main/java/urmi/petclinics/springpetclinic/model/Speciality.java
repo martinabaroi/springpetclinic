1726e322ea4223a399e90d437acb8dede797cabc
martinabaroi <109995362+martinabaroi@users.noreply.github.com>
2022-09-07 11:35:04 -0400

    convert-vet-jpa-entity (#43)

--- pet-clinic-data/src/main/java/urmi/petclinics/springpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/urmi/petclinics/springpetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package urmi.petclinics.springpetclinic.model;
 
-public class Speciality extends BaseEntity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "specialities")
+public class Speciality extends BaseEntity {
+	
+	@Column(name = "discription")
 	private String discrption;
 
 	public String getDiscrption() {
--- pet-clinic-data/src/main/java/urmi/petclinics/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/urmi/petclinics/springpetclinic/model/Vet.java
@@ -3,8 +3,22 @@ package urmi.petclinics.springpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+				inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<Speciality> specialities = new HashSet<>();  //id, discription
 
 	public Set<Speciality> getSpecialities() {

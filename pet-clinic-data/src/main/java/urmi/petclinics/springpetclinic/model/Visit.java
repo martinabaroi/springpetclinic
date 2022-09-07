a2b69d6d48a82322bc0ce0f85c3c77c58dcbff5d
Martina Baroi <m_baroi@yahoo.com>
2022-09-07 11:48:34 -0400

    create-visit-entity

--- pet-clinic-data/src/main/java/urmi/petclinics/springpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/urmi/petclinics/springpetclinic/model/Visit.java
@@ -2,10 +2,24 @@ package urmi.petclinics.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Visit extends BaseEntity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "visits")
+public class Visit extends BaseEntity {
+	
+	@Column(name = "date")
 	private LocalDate date; 
+	
+	@Column(name = "discription")
 	private String discription;
+	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 	
 	

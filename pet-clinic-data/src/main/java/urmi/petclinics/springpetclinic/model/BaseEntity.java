c88814fc3551d3144f54b6f5de4b7a90faa4f988
martinabaroi <109995362+martinabaroi@users.noreply.github.com>
2022-09-07 10:22:43 -0400

    base entity modification (#41)

--- pet-clinic-data/src/main/java/urmi/petclinics/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/urmi/petclinics/springpetclinic/model/BaseEntity.java
@@ -2,8 +2,17 @@ package urmi.petclinics.springpetclinic.model;
 
 import java.io.Serializable;
 
-public class BaseEntity implements Serializable {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass		//Instead of putting all these properties in every entity, we can place these properties in a base entity (annotate base entity with MappedSuperclass annotation).	
+						//we dont need any table in database for this baseenity so didnt write @Entity.
+public class BaseEntity implements Serializable {  
+
+	@Id				//when any entity extends this class. that entity primary key will be this id and id will be auto generated so put @Id anootation here.
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Long id;
 
 	public Long getId() {
